afe8f01156d5f9273b20661814421b662977ac31
andicretu <149256225+andicretu@users.noreply.github.com>
2024-02-13 14:49:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,33 +5,18 @@ array_colors = ["btn-danger", "btn-warning", "btn-success"];
 let colorRange = 0;
 
 let button = document.getElementById('TLContainer');
-button.innerHTML  = `
-<div class="d-grid gap-2">
-  <button type="button" class="btn ${array_colors[colorRange]} btn-lg">Click me</button>
-</div>
-<br>`;
 
 function changeColor() {
-   if (colorRange < 3) {
-    button.querySelector('button').className = `btn ${array_colors[colorRange]} btn-lg`;
-    ++colorRange
-   } else {
-    colorRange = 0;
-    clearInterval(intervalID);
-    runTime();
-    button.querySelector('button').className = `btn ${array_colors[colorRange]} btn-lg`;
+    if (colorRange < 3) {
+        button.querySelector('button').className = `btn ${array_colors[colorRange]} btn-lg`;
+        ++colorRange;
+    } else {
+        colorRange = 0; // daca nu resetez variabila asta, butonul ramane incolor dupa terminarea ciclului.
+        button.querySelector('button').className = `btn ${array_colors[colorRange]} btn-lg`;
    }
 }
 
-function increaseTime() {
-    time += 1;
-    let seconds = Math.floor(time % 1000 / 10)
-}
-function runTime() {
-   intervalID = setInterval(increaseTime, 100)
-}
 window.addEventListener('load', function() {
-    runTime();
     changeColor();
     setInterval(changeColor, 10000);
 });
